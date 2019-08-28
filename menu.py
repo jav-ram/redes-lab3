8ca039598f2f0324ea70aa4386643869677f8ebe
Gadhi <pablogadhi@gmail.com>
2019-08-28 16:18:17 -0600

    Menu

--- menu.py
+++ menu.py
@@ -6,11 +6,14 @@ t = Terminal()
 
 
 def handle_table(self):
-        print("handle_table flooding")
+    print("handle_table flooding")
 
 
 def OptionsMenu(send_individual_message, handle_table):
     while True:
+        print("\nOpciones:")
+        print("msg - Mandar Mensaje")
+        print("table - Ver Tabla\n")
         o = input("Option: ")
         if o == 'msg':
             send_individual_message()
